a110e9d80a76eba6b5787bf9b6e9db6d1483cb8b
Tran Dong <trandong2932002@gmail.com>
2024-06-30 02:49:05 +0700

    refactor: replace_emails, replace_urls

--- src/urls/mod.rs
+++ src/urls/mod.rs
@@ -1,24 +1,24 @@
 use aho_corasick::{AhoCorasick, MatchKind};
-use linkify::{Link, LinkFinder, LinkKind};
+use linkify::{LinkFinder, LinkKind};
 
 /// Filter out all emails from text, using Aho-Corasick algorithm.
 pub fn replace_emails(text: &str) -> String {
     let mut finder = LinkFinder::new();
     finder.kinds(&[LinkKind::Email]);
 
-    let links = finder.links(&text).collect::<Vec<Link>>();
-    let links = links
+    let emails = finder
+        .links(&text)
         .into_iter()
         .map(|link| link.as_str())
         .collect::<Vec<&str>>();
-    let links_replace = vec![" (email) "; links.len()];
+    let emails_replacement = vec![" (email) "; emails.len()];
 
     let ac = AhoCorasick::builder()
         .ascii_case_insensitive(true)
         .match_kind(MatchKind::LeftmostLongest)
-        .build(&links)
+        .build(&emails)
         .unwrap();
-    ac.replace_all(&text, &links_replace)
+    ac.replace_all(&text, &emails_replacement)
 }
 
 /// Filter out all urls from text, using Aho-Corasick algorithm.
@@ -27,17 +27,17 @@ pub fn replace_urls(text: &str) -> String {
     finder.kinds(&[LinkKind::Url]);
     finder.url_must_have_scheme(false);
 
-    let links = finder.links(&text).collect::<Vec<Link>>();
-    let links = links
+    let links = finder
+        .links(&text)
         .into_iter()
         .map(|link| link.as_str())
         .collect::<Vec<&str>>();
-    let links_replace = vec![" (url) "; links.len()];
+    let links_replacement = vec![" (url) "; links.len()];
 
     let ac = AhoCorasick::builder()
         .ascii_case_insensitive(true)
         .match_kind(MatchKind::LeftmostLongest)
         .build(&links)
         .unwrap();
-    ac.replace_all(&text, &links_replace)
+    ac.replace_all(&text, &links_replacement)
 }
