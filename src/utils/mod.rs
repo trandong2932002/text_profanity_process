aee6ad6605dd3113e79f26ae703b2b700aa99cd5
Tran Dong <trandong2932002@gmail.com>
2024-07-01 05:07:18 +0700

    refactor: utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,42 +1,22 @@
+use aho_corasick::{AhoCorasick, MatchKind};
 use std::collections::HashMap;
 
-use aho_corasick::{AhoCorasick, MatchKind};
-use polars::{datatypes::StringChunked, error::PolarsError, prelude::*, series::Series};
-
-// todo: rewrite using macro
-pub fn series_str_map(
-    series_str: Series,
-    function: fn(&str) -> String,
-) -> Result<Option<Series>, PolarsError> {
-    let result = series_str
-        .str()
-        .unwrap()
-        .into_iter()
-        .map(|opt_str: Option<&str>| opt_str.map(|text: &str| function(text)))
-        .collect::<StringChunked>();
-    Ok(Some(result.into_series()))
-}
-
-// polars replace_many cannot configure MatchKind option, so I wrote my own
-pub fn series_str_replace_all(
-    series_str: Series,
-    replace_hashmap: HashMap<String, String>,
-) -> Result<Option<Series>, PolarsError> {
-    let patterns = replace_hashmap.clone().into_keys().collect::<Vec<String>>();
-    let replace_with = replace_hashmap
+pub fn aho_corasick_replace_all(
+    text: &str,
+    replacement_hashmap: &HashMap<String, String>,
+) -> String {
+    let patterns = replacement_hashmap
+        .clone()
+        .into_keys()
+        .collect::<Vec<String>>();
+    let replace_with = replacement_hashmap
         .clone()
         .into_values()
         .collect::<Vec<String>>();
     let ac = AhoCorasick::builder()
         .ascii_case_insensitive(true)
         .match_kind(MatchKind::LeftmostLongest)
-        .build(&patterns)
+        .build(patterns)
         .unwrap();
-    let result = series_str
-        .str()
-        .unwrap()
-        .into_iter()
-        .map(|opt_str: Option<&str>| opt_str.map(|text: &str| ac.replace_all(&text, &replace_with)))
-        .collect::<StringChunked>();
-    Ok(Some(result.into_series()))
+    ac.replace_all(&text, &replace_with)
 }
