c261f92ee479a4af01d1476af0a118efad3bd5c2
Tran Dong <trandong2932002@gmail.com>
2024-07-01 05:07:48 +0700

    refactor: other patterns

--- src/other_patterns/mod.rs
+++ src/other_patterns/mod.rs
@@ -1,60 +1,56 @@
-use std::{collections::HashMap, fs::File, io::BufReader};
+use once_cell::sync::Lazy;
+use std::{collections::HashMap, fmt::Write, fs::File, io::BufReader};
 
-/// Get HashMap of English contractions.
-pub fn get_english_contractions_hashmap() -> HashMap<String, String> {
-    let mut english_contractions = HashMap::new();
-    english_contractions.insert("won't".to_owned(), "will not".to_owned());
-    english_contractions.insert("can't".to_owned(), "can not".to_owned());
-    english_contractions.insert("n't".to_owned(), " not".to_owned());
-    english_contractions.insert("'re".to_owned(), " are".to_owned());
-    english_contractions.insert("'s".to_owned(), " is".to_owned());
-    english_contractions.insert("'d".to_owned(), " would".to_owned());
-    english_contractions.insert("'ll".to_owned(), " will".to_owned());
-    english_contractions.insert("'t".to_owned(), " not".to_owned());
-    english_contractions.insert("'ve".to_owned(), " have".to_owned());
-    english_contractions.insert("'m".to_owned(), " am".to_owned());
-    english_contractions
-}
+use crate::aho_corasick_replace_all;
 
-/// Get HashMap of Wikipedia shortcuts.
-pub fn get_wikipedia_shortcuts_hashmap() -> HashMap<String, String> {
+pub const WIKIPEDIA_NAMESPACE_REGEX: &str = r#"(talk|user|wikipedia|wp|project|wt|template|tm|help|category|portal|draft|timedtext|module|special|topic|education program|book|gadget|gadget definition)((_| )talk)?:[\w\/#]+"#;
+pub const WIKIPEDIA_FILE_NAMESPACE_REGEX: &str =
+    r#"(file|image)((_| )talk)?:([\w\s\(\)\&\-\"\']+)((\.(\w{3}))|,|\.|\)|\")"#;
+
+static ENGLISH_CONTRACTIONS: Lazy<HashMap<String, String>> = Lazy::new(|| {
+    let result = HashMap::from([
+        ("won't", "will not"),
+        ("can't", "can not"),
+        ("n't", " not"),
+        ("'re", " are"),
+        ("'s", " is"),
+        ("'d", " would"),
+        ("'ll", " will"),
+        ("'t", " not"),
+        ("'ve", " have"),
+        ("'m", " am"),
+    ]);
+    result
+        .into_iter()
+        .map(|(key, value)| (key.to_owned(), value.to_owned()))
+        .collect()
+});
+
+static WIKIPEDIA_SHORTCUTS: Lazy<HashMap<String, String>> = Lazy::new(|| {
     let wikipedia_shortcuts_filepath = "data/others/wiki_shortcuts.json";
     let file = File::open(wikipedia_shortcuts_filepath).unwrap();
     let reader = BufReader::new(file);
     let wikipedia_shortcuts: HashMap<String, String> = serde_json::from_reader(reader).unwrap();
-    let keys = get_wikipedia_shortcuts_keys_hashmap(&wikipedia_shortcuts);
-    let values = get_wikipedia_shortcuts_values_hashmap(&wikipedia_shortcuts);
-    let mut result = keys;
-    result.extend(values);
+    let keys = wikipedia_shortcuts
+        .iter()
+        .map(|(key, _)| (key.clone(), " (wikipedia shortcut) ".to_owned()))
+        .collect::<HashMap<String, String>>();
+    let values = wikipedia_shortcuts
+        .iter()
+        .map(|(_, value)| (value.clone(), " (wikipedia shortcut) ".to_owned()))
+        .collect::<HashMap<String, String>>();
+    let mut result = HashMap::new();
+    result.extend(keys.to_owned());
+    result.extend(values.to_owned());
     result
+});
+
+pub fn replace_english_contractions(text: &str, output: &mut String) {
+    let result = aho_corasick_replace_all(text, &ENGLISH_CONTRACTIONS);
+    write!(output, "{}", result).unwrap();
 }
 
-fn get_wikipedia_shortcuts_keys_hashmap(
-    wikipedia_shortcuts: &HashMap<String, String>,
-) -> HashMap<String, String> {
-    wikipedia_shortcuts
-        .clone()
-        .into_keys()
-        .map(|keys| (keys, " (wikipedia shortcut) ".to_owned()))
-        .collect()
-}
-
-fn get_wikipedia_shortcuts_values_hashmap(
-    wikipedia_shortcuts: &HashMap<String, String>,
-) -> HashMap<String, String> {
-    wikipedia_shortcuts
-        .clone()
-        .into_values()
-        .map(|values| (values, " (wikipedia shortcut) ".to_owned()))
-        .collect()
-}
-
-/// Get regex of Wikipedia namespaces.
-pub fn get_wikipedia_namespace_regex() -> String {
-    r#"(talk|user|wikipedia|wp|project|wt|template|tm|help|category|portal|draft|timedtext|module|special|topic|education program|book|gadget|gadget definition)((_| )talk)?:[\w\/#]+"#.to_owned()
-}
-
-/// Get regex of Wikipedia file namespaces.
-pub fn get_wikipedia_file_namespace_regex() -> String {
-    r#"(file|image)((_| )talk)?:([\w\s\(\)\&\-\"\']+)((\.(\w{3}))|,|\.|\)|\")"#.to_owned()
+pub fn replace_wikipedia_shortcuts(text: &str, output: &mut String) {
+    let result = aho_corasick_replace_all(text, &WIKIPEDIA_SHORTCUTS);
+    write!(output, "{}", result).unwrap();
 }
