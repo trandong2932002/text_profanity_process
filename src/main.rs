0b153b49b4514799118972fb9d4381b31672b26b
Tran Dong <trandong2932002@gmail.com>
2024-07-06 22:01:31 +0700

    vietnamese preprocess

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use std::{env, time::Instant};
+use std::io;
 
 use crate::emojis::*;
-use other_patterns::*;
-use polars::{
-    lazy::dsl::{col, lit, GetOutput},
-    prelude::*,
-};
 use spelling_corrector::*;
 use unicode::*;
+use unicode_normalization::UnicodeNormalization;
 use urls::{replace_emails, replace_urls};
 use utils::*;
 
@@ -19,142 +15,28 @@ mod urls;
 mod utils;
 
 fn main() {
-    let now = Instant::now();
-    // main
-    env::set_var("POLARS_FMT_STR_LEN", "120");
-    let dataset_filepath =
-        "data/trainning_dataset/jigsaw-toxic-comment-classification-challenge/train.csv";
+    loop {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        line = line.trim().to_lowercase();
+        line = line.nfkc().collect::<String>();
+        line = apply(&line, replace_emails);
+        line = apply(&line, replace_urls);
+        line = apply(&line, replace_emoticons);
+        line = apply(&line, replace_unicode_emojis);
+        line = apply(&line, unicode_filter_by_blocks);
+        line = apply(&line, unicode_filter_by_categories);
+        line = apply(&line, process_text);
 
-    let mut df = LazyCsvReader::new(dataset_filepath)
-        .with_has_header(true)
-        .finish()
-        .unwrap()
-        .with_column(col("comment_text").alias("m_ct"))
-        // replace ip addresses
-        .with_column(col("m_ct").str().replace_all(
-            lit(r"((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)"),
-            lit(r" (ip address) "),
-            false,
-        ))
-        // replace emails
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(replace_emails);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        // replace urls
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(replace_urls);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        // replace time (not date)
-        .with_column(col("m_ct").str().replace_all(
-            lit(r"(([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?)"),
-            lit(r" (time) "),
-            false,
-        ))
-        //? replace number with name?
-        // replace english contractions
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(replace_english_contractions);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        // replace wikipedia: shortcuts, (file) namespaces
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(replace_wikipedia_shortcuts);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        .with_column(col("m_ct").str().replace_all(
-            lit(WIKIPEDIA_NAMESPACE_REGEX),
-            lit(r" (wikipedia namespace) "),
-            false,
-        ))
-        .with_column(col("m_ct").str().replace_all(
-            lit(WIKIPEDIA_FILE_NAMESPACE_REGEX),
-            lit(r" (wikipedia file namespace) "),
-            false,
-        ))
-        // replace emoticons/emojis
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(replace_emoticons);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(replace_unicode_emojis);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        // unicode filter: categories, blocks
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(unicode_filter_by_blocks);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(unicode_filter_by_categories);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        // unicode: decode
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(unicode_decode);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        // split punctuations/symbols around word
-        .with_column(col("m_ct").str().replace_all(
-            lit(r"\b([\w\d]?[^\s]*[\w\d]?)\b"),
-            lit(r" ${1} "),
-            false,
-        ))
-        // other process
-        .with_column(col("m_ct").map(
-            |s| {
-                let ca = s.str()?;
-                let out = ca.apply_to_buffer(process_text);
-                Ok(Some(out.into_series()))
-            },
-            GetOutput::same_type(),
-        ))
-        .collect()
-        .unwrap();
-    let mut file = std::fs::File::create("output.csv").unwrap();
-    CsvWriter::new(&mut file).finish(&mut df).unwrap();
-    // let row = df.get_row(0).unwrap();
-    // println!("{:?}", row.0[1]);
-    // println!("{:?}", row.0[2]);
-    //
-    let elapsed = now.elapsed();
-    println!("{:?}", elapsed);
+        println!("{}", line);
+    }
+}
+
+fn apply<'a, F>(line: &'a str, mut f: F) -> String
+where
+    F: FnMut(&'a str, &mut String),
+{
+    let mut buf = String::new();
+    f(line, &mut buf);
+    buf
 }
--- src/spelling_corrector/mod.rs
+++ src/spelling_corrector/mod.rs
@@ -1,5 +1,4 @@
 use std::{
-    borrow::Borrow,
     collections::HashSet,
     fmt::Write,
     fs::File,
@@ -8,39 +7,26 @@ use std::{
 
 use aho_corasick::{AhoCorasick, MatchKind};
 use once_cell::sync::Lazy;
-use rust_stemmers::{Algorithm, Stemmer};
 use symspell::{SymSpell, UnicodeStringStrategy, Verbosity};
 
 use crate::get_unicode_category;
 
 const BIGRAM_DUPLICATE_THRESHOLD: f32 = 0.3; // magic number
-
-// const ENGLISH_ONE_LETTER_WORDS: [char; 12] =
-//     ['a', 'i', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-// const ENGLISH_TWO_LETTERS_WORDS: [&str; 26] = [
-//     "am", "an", "as", "at", "be", "by", "do", "go", "he", "if", "in", "is", "it", "me", "my", "no",
-//     "of", "ok", "on", "or", "ox", "so", "to", "up", "us", "we",
-// ];
-// const ENGLISH_AVG_WORD_LENGTH: i32 = 5;
-// const ENGLISH_WORD_LENGTH_THRESHOLD: i32 = 2 * ENGLISH_AVG_WORD_LENGTH;
-const ENGLISH_SEGMENTATION_MAX_EDIT_DISTANCE: i64 = 2;
+const SEGMENTATION_MAX_EDIT_DISTANCE: i64 = 2;
 
 pub static SYMSPELL: Lazy<SymSpell<UnicodeStringStrategy>> = Lazy::new(|| {
     eprintln!("Spelling Corrector: SymSpell loading...");
     let mut spell = SymSpell::default();
-    let english_frequency_filepath = "data/dictionaries/english/frequency_dictionary_en_82_765.txt";
-    let english_bigram_frequency_filepath =
-        "data/dictionaries/english/frequency_bigramdictionary_en_243_342.txt";
-    spell.load_dictionary(&english_frequency_filepath, 0, 1, " ");
-    spell.load_bigram_dictionary(&english_bigram_frequency_filepath, 0, 2, " ");
+    let vietnamese_frequency_filepath = "data/dictionaries/vietnamese/vi_50k.txt";
+    spell.load_dictionary(&vietnamese_frequency_filepath, 0, 1, " ");
     spell
 });
 
-static ENGLISH_DICTIONARY: Lazy<HashSet<String>> = Lazy::new(|| {
-    eprintln!("Spelling Corrector: English dictionary loading...");
+static VIETNAMESE_DICTIONARY: Lazy<HashSet<String>> = Lazy::new(|| {
+    eprintln!("Spelling Corrector: Vietnamese dictionary loading...");
     let mut dictionary: HashSet<String> = HashSet::new();
-    let english_dictionary_filepath = "data/dictionaries/english/words_alpha.txt";
-    let file = File::open(english_dictionary_filepath).unwrap();
+    let vietnamese_dictionary_filepath = "data/dictionaries/vietnamese/words_alpha.txt";
+    let file = File::open(vietnamese_dictionary_filepath).unwrap();
     let reader = BufReader::new(file);
     for line in reader.lines() {
         dictionary.insert(line.unwrap());
@@ -71,32 +57,22 @@ static ENGLISH_SWEAR_WORDS_REPLACEMENT: Lazy<Vec<String>> = Lazy::new(|| {
         .collect()
 });
 
-static ENGLISH_FIRSTNAMES: Lazy<Vec<String>> = Lazy::new(|| {
-    eprintln!("Spelling Corrector: English firtnames loading...");
-    let mut firstnames: Vec<String> = Vec::new();
-    let english_firstnames_filepath = "data/dictionaries/english/first-names.txt";
-    let file = File::open(english_firstnames_filepath).unwrap();
+static VIETNAMESE_SWEAR_WORDS: Lazy<Vec<String>> = Lazy::new(|| {
+    eprintln!("Spelling Corrector: Vietnamese swear words loading...");
+    let vietnamese_swear_words_filepath = "data/dictionaries/vietnamese/vi.json";
+    let file = File::open(vietnamese_swear_words_filepath).unwrap();
     let reader = BufReader::new(file);
-    for line in reader.lines() {
-        let line = line.unwrap().to_lowercase();
-        firstnames.push(line);
-    }
-    firstnames
+    serde_json::from_reader(reader).unwrap()
 });
 
-static ENGLISH_FIRSTNAMES_REPLACEMENT: Lazy<Vec<String>> = Lazy::new(|| {
-    eprintln!("Spelling Corrector: English swear words replacement creating...");
-    ENGLISH_FIRSTNAMES
+static VIETNAMESE_SWEAR_WORDS_REPLACEMENT: Lazy<Vec<String>> = Lazy::new(|| {
+    eprintln!("Spelling Corrector: Vietnamese swear words replacement creating...");
+    VIETNAMESE_SWEAR_WORDS
         .clone()
         .into_iter()
         // .filter(|word| !word.contains(" "))
         .map(|word| format!(" {} ", word))
         .collect()
-});
-
-static ENGLISH_STEMMER: Lazy<Stemmer> = Lazy::new(|| {
-    eprintln!("Spelling Corrector: English stemmer loading...");
-    Stemmer::create(Algorithm::English)
 });
 
 /// Create vector of bigrams, e.g. hello => he,el,ll,lo
@@ -163,7 +139,7 @@ fn reduce_bigram(word: &str) -> String {
 
 /// Check if a word is in the corpora.
 fn is_in_corpora(word: &str) -> bool {
-    ENGLISH_DICTIONARY.contains(word)
+    VIETNAMESE_DICTIONARY.contains(word)
 }
 
 /// Check if a word is a number.
@@ -181,7 +157,7 @@ fn is_math_equation(word: &str) -> bool {
         .all(|letter| ['N', 'S', 'P'].contains(&get_unicode_category(&letter)))
 }
 
-// Check if a word contains only punctuations and symbols.
+/// Check if a word contains only punctuations and symbols.
 fn is_punctuations_or_symbols(word: &str) -> bool {
     word.chars()
         .into_iter()
@@ -202,26 +178,6 @@ pub fn correct_unknown_word(word: &str) -> String {
             }
         })
         .collect::<String>();
-    if new_word != word {
-        let mut result_words: Vec<String> = Vec::new();
-        let mut can_correct_flag = true;
-        for _word in new_word.split_whitespace().into_iter() {
-            if is_in_corpora(_word) {
-                result_words.push(_word.to_owned());
-                continue;
-            }
-            let suggestion = SYMSPELL.lookup(_word, Verbosity::Top, 2);
-            if suggestion.len() == 0 {
-                can_correct_flag = false;
-                break;
-            }
-            result_words.push(suggestion[0].term.to_owned());
-        }
-        if can_correct_flag {
-            // print!("1>");
-            return result_words.join(" ");
-        }
-    }
     // second: this word may be complex (multiple words and wrong spell), split and try to correct
     // when split misspelling word, the result may contains unknown sequences of 1/2-chars words (not yet impl)
     // ex: he.l.loh.o.w.ar.ey.ou
@@ -233,19 +189,13 @@ pub fn correct_unknown_word(word: &str) -> String {
     let ac = AhoCorasick::builder()
         .ascii_case_insensitive(true)
         .match_kind(MatchKind::LeftmostLongest)
-        .build(&ENGLISH_SWEAR_WORDS.to_owned())
+        // .build(&ENGLISH_SWEAR_WORDS.to_owned())
+        .build(&VIETNAMESE_SWEAR_WORDS.to_owned())
         .unwrap();
-    new_word = ac.replace_all(&new_word, &ENGLISH_SWEAR_WORDS_REPLACEMENT);
-    // replace firstnames
-    let ac = AhoCorasick::builder()
-        .ascii_case_insensitive(true)
-        .match_kind(MatchKind::LeftmostLongest)
-        .build(&ENGLISH_FIRSTNAMES.to_owned())
-        .unwrap();
-    new_word = ac.replace_all(&new_word, &ENGLISH_FIRSTNAMES_REPLACEMENT);
+    new_word = ac.replace_all(&new_word, &VIETNAMESE_SWEAR_WORDS_REPLACEMENT);
     // split text
     let segmented_string = SYMSPELL
-        .word_segmentation(&new_word, ENGLISH_SEGMENTATION_MAX_EDIT_DISTANCE)
+        .word_segmentation(&new_word, SEGMENTATION_MAX_EDIT_DISTANCE)
         .segmented_string;
     segmented_string
 }
@@ -270,7 +220,7 @@ pub fn process_text(text: &str, output: &mut String) {
             result_words.push(word);
             continue;
         }
-        if is_in_corpora(ENGLISH_STEMMER.stem(&word).borrow()) {
+        if is_in_corpora(&word) {
             result_words.push(word);
             continue;
         }
